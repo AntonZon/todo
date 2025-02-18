fbfd2f6507349e9d515a4298549e4fb91f5ad060
Anton Onanchenko <onanchenko@gmail.com>
2025-02-18 22:37:37 +0100

    implemented 7 tests simplebookapi

--- tests/simplebooksapi.test.js
+++ tests/simplebooksapi.test.js
@@ -1,9 +1,18 @@
 //tests for https://simple-books-api.glitch.me/api/books
 const axios = require('axios');
 
-const fetchData = async (url) => {
+const fetchData = async (url, config) => {
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(url, config);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const postData = async (url, data, config) => {
+    try {
+        const response = await axios.post(url, data, config);
         return response;
     } catch (error) {
         throw error;
@@ -11,6 +20,9 @@ const fetchData = async (url) => {
 };
 
 const baseURL = "https://simple-books-api.glitch.me";
+const randomEmail = Math.random().toString(36).substring(7) + "@test.com";
+let token;
+let orderId;
 
 describe('Simple book tests', () => {
 
@@ -25,6 +37,111 @@ describe('Simple book tests', () => {
             // Assert
             expect(response.status).toBe(200);
             expect(response.statusText).toBe("OK");
+        });
+    });
+
+    describe('Check List Of Books', () => {
+
+        it('fetches user data successfully', async () => {
+            // Arrange
+
+            // Act
+            const response = await fetchData(baseURL + "/books");
+
+            // Assert
+            expect(response.status).toBe(200);
+        });
+    });
+
+    describe('Check Get Book', () => {
+
+        it('fetches user data successfully', async () => {
+            // Arrange
+            const bookId = 1;
+            // Act
+            const response = await fetchData(baseURL + "/books/" + bookId);
+
+            // Assert
+            expect(response.status).toBe(200);
+        });
+    });
+
+    describe('Authorization (POST)', () => {
+
+        it('Authorization successfully completed', async () => {
+            // Arrange
+
+            // Act
+            const response = await postData(baseURL + "/api-clients", {
+                "clientName": "Postman",
+                "clientEmail": randomEmail,
+            });
+
+            // Assert
+            expect(response.status).toBe(201);
+            expect(response.data).toHaveProperty("accessToken");
+            token = response.data.accessToken;
+        });
+    });
+
+    describe('Order book', () => {
+
+        it('Order book successfully', async () => {
+            // Arrange
+
+            // Act
+            const response = await postData(baseURL + "/orders", {
+                "bookId": 5,
+                "customerName": "Anton"
+            }, {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+
+            // Assert
+            expect(response.status).toBe(201);
+            expect(response.data).toHaveProperty("orderId");
+            orderId = response.data.orderId;
+        });
+    });
+   
+    describe('Check Get orders', () => {
+
+        it('fetches orders successfully', async () => {
+            // Arrange
+            
+            // Act
+            const response = await fetchData(baseURL + "/orders", {
+                headers: { Authorization: `Bearer ${token}` }
+            });
+            // Assert
+            expect(response.status).toBe(200);
+        });
+    });
+    
+ /*   describe('Check patch the order', () => {
+
+        it('fetches user order successfully', async () => {
+            // Arrange
+            const bookId = 1;
+            // Act
+            const response = await fetchData(baseURL + `/orders/${orderId}`, {
+                headers: { Authorization: `Bearer ${token}`}});
+            // Assert
+            expect(response.status).toBe(200);
+        });
+    });
+*/
+
+    describe('Check Get The Order', () => {
+
+        it('fetches user order successfully', async () => {
+            // Arrange
+            const bookId = 1;
+            // Act
+            const response = await fetchData(baseURL + `/orders/${orderId}`, {
+                headers: { Authorization: `Bearer ${token}`}});
+            // Assert
+            expect(response.status).toBe(200);
         });
     });
 });
